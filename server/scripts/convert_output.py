26ee2c422fe636967feba98113dfbcacd7b73ddd
David Banik <53061767+DaveBane@users.noreply.github.com>
2025-01-31 22:53:45 -0800

    Delete server/scripts/convert_output.py

--- server/scripts/convert_output.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def convert_to_json():
-    with open("output.json", "r") as f:
-        data = json.load(f)
-    with open("../public/simulation_data.json", "w") as f:
-        json.dump(data, f)
-
-convert_to_json()
