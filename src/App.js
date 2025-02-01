9f7db21daccfc5176ab7b05568f58ce86b174165
David Banik <53061767+DaveBane@users.noreply.github.com>
2025-01-31 23:01:40 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Simulation from "./components/Simulation";
+
+function App() {
+  return (
+    <div>
+      <h1>3D Sphere Nucleation Simulation</h1>
+      <Simulation />
+    </div>
+  );
+}
+
+export default App;
