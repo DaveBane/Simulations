b6c8a54b7a62d46850112be9fc1246018839855a
David Banik <53061767+DaveBane@users.noreply.github.com>
2025-01-31 22:42:51 -0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Simulation from '../src/components/Simulation';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>3D Sphere Nucleation Simulation</h1>
+      <Simulation />
+    </div>
+  );
+}
